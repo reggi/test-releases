0ca17eee7644686fa799a41c664cb456741f2190
reggi <thomas@reggi.com>
2015-01-06 17:43:39 -0500

    special file

--- release.sh
+++ release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 VERSION=$1
 APP_NAME="Holstee"
 APP_NAME_LOWERCASE=`echo $APP_NAME | tr '[:upper:]' '[:lower:]'`
-CHANGELOG=`cat changelog-$VERSON.md`
+
 export GITHUB_TOKEN=e1ee771642938433c8215a48e034173fa7b4016b
 export GITHUB_USER=reggi
 export GITHUB_REPO=test-releases
@@ -10,10 +11,13 @@ export GITHUB_REPO=test-releases
 git tag $VERSION
 git push --tags
 
+LATEST_VERSION=`git log --pretty="format:%d" | head -n2 | tail -n1 | sed 's/[()]//g' | sed  's ......  '`
+CHANGELOG=`git log --pretty=oneline ^$LATEST_VERSION $VERSION | sed -E -l -e 's/........................................ (.+)/* \1/'`
+
 github-release release \
   --tag $VERSION \
   --name $APP_NAME" "$VERSION \
-  --description $CHANGELOG \
+  --description "$CHANGELOG" \
 
 github-release upload \
   --tag $VERSION \
---
+++ special_file.md
@@ -0,0 +1,1 @@
+new v10 file
